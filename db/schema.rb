8b4cef7846484be9a6f6d5debe3865e7dddb4680
codewurld <sholaqdev@gmail.com>
2023-05-17 18:28:22 +0100

    adds assets and custom css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_165235) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_171857) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_165235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
-    t.date "release_date"
+    t.date "released_on"
   end
 
 end
