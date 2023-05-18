16510f4850d2ccd57ac6cb5be090141fb30a4650
codewurld <sholaqdev@gmail.com>
2023-05-18 18:37:00 +0100

    links pages and adds functionality to edit and update movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,21 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def show
+        @movie = Movie.find(params[:id]);
+    end
+
+    def edit
+        @movie = Movie.find(params[:id]);
+    end
+
+    def update
+        @movie = Movie.find(params[:id]);
+        # specifies attributes we want to permit for update
+        event_param = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross);
+        @movie.update(event_param)
+        # redirect to show view
+        redirect_to movie_path(@movie)
+    end
 end
