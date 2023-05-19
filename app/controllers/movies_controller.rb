c0f96606e160b8b896196d4f7aaeea7cd5e5a732
codewurld <sholaqdev@gmail.com>
2023-05-19 12:12:34 +0100

    refactors code and adds new movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,10 +13,25 @@ class MoviesController < ApplicationController
 
     def update
         @movie = Movie.find(params[:id]);
-        # specifies attributes we want to permit for update
-        event_param = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross);
         @movie.update(event_param)
         # redirect to show view
         redirect_to movie_path(@movie)
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(event_param)
+        @movie.save
+        redirect_to movie_path(@movie)
+    end
+
+    private
+
+    # specifies attributes we want to permit for update
+    def event_param
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross);
     end
 end
