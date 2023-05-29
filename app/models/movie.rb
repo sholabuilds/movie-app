66dfe7bf6e6de7fd78c884d03a2f9f2cd0123a1d
codewurld <sholaqdev@gmail.com>
2023-05-29 13:17:04 +0100

    renders ony previously released movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ApplicationRecord
+    def self.prev_released
+        where("released_on < ?", Time.now).order("released_on desc")
+    end
+    
     def flop?
         total_gross.blank? || total_gross < 200000000
     end
