862717b2846bd40a61611cc827ec1a3de379a48b
sholabuilds <sholabuildingstuff@gmail.com>
2023-06-08 12:08:55 +0100

    enables user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    # find user and authenticate
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+    
+    if user && user.authenticate(params[:password])
+      # store user ID in hash
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back #{user.username}!"
+    else
+      flash.now[:alert] = "Incorrect email/password combination!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    # current_user is used globally
+    def current_user
+        User.find(session[:user_id]) if session[:user_id]
+    end
 end
